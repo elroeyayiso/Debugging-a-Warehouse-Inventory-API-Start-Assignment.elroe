fe04640fea4e7e98badabf7ed2a5152d981f30fc
elroeyayiso <86745767+elroeyayiso@users.noreply.github.com>
2023-07-25 15:49:22 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,104 @@
+const express = require('express'); // ### Q1 ### get the "express" module
+const cors = require('cors'); // ### Q2 ### get the "cors" module
+const port = 3000;
+const app = express();
+
+// for parsing application/json requests
+app.use(express.json());
+// for parsing application/x-www-form-urlencoded requests
+app.use(express.urlencoded({ extended: true }));
+// for allowing different domain origins to make requests to this API
+app.use(cors());
+
+const inventory = {
+  items: [
+    {
+      name: 'socks',
+      count: 10
+    },
+    {
+      name: 'pants',
+      count: 10
+    },
+    // ### Q3 ### Add inventory entry for "shirt" with a stock of 10
+    {
+      name: 'shirt',
+      count: 10
+    }
+  ],
+};
+
+// Retrieve all stock route
+app.get('/inventory/retrieve-all', (req, res) => {
+  res.json(inventory);
+});
+
+// Retrieve stock route
+app.get('/inventory/retrieve/:itemname', (req, res) => { // ### Q4 ### Uncomment the code below and Fix the function call which listens for GET HTTP requests
+  const item = inventory.items.find(item => item.name === req.params.itemname.trim().toLowerCase());
+
+  if (!item) {
+    res.status(404).json({
+      itemname: req.params.itemname,
+      error: 'Could not find the item in inventory'
+    });
+  } else {
+    res.json(item);
+  }
+});
+
+// Add stock route
+app.post('/inventory/add', (req, res) => { // ### Q5 ### Uncomment the code below and Fix the function call which listens for POST HTTP requests
+  const item = inventory.items.find(item => item.name === req.body.name.trim().toLowerCase());
+
+  if (item) {
+    res.status(409).json({
+      itemname: req.body.name,
+      error: 'Item already exists in inventory'
+    });
+  } else {
+    inventory.items.push({
+      name: req.body.name,
+      count: req.body.count
+    });
+
+    res.status(201).json(inventory);
+  }
+});
+
+// Change stock route
+app.put('/inventory/change', (req, res) => { // ### Q6 ### Uncomment the code below and Fix the function call which listens for PUT HTTP requests
+  const itemIndex = inventory.items.findIndex(item => item.name === req.body.name.trim().toLowerCase());
+
+  if (itemIndex === -1) {
+    res.status(404).json({
+      itemname: req.body.name,
+      error: 'Could not find the item in inventory'
+    });
+  } else {
+    inventory.items[itemIndex].count += req.body.change;
+
+    res.status(200).json(inventory);
+  }
+});
+
+// Remove stock route
+app.delete('/inventory/remove/:name', (req, res) => { // ### Q8 ### Uncomment the code below and Fix the function call which listens for DELETE HTTP requests
+  const itemIndex = inventory.items.findIndex(item => item.name === req.params.name.trim().toLowerCase()); // ### Q9 ### Also Fix the parameter route, so the item's name can be retrieved correctly
+
+  if (itemIndex === -1) {
+    res.status(404).json({
+      itemname: req.params.name,
+      error: 'Could not find the item in inventory'
+    });
+  } else {
+    inventory.items.splice(itemIndex, 1);
+    res.status(200).json(inventory);
+  }
+});
+
+// Start Express server to listen for API connections
+app.listen(port, () => console.log(`API listening at http://localhost:${port}/`));
+
+
+
